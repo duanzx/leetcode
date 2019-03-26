bf06ce5bf4c49ce1d71e3756eedf43baafeab6d2
duanzx <15726645490@163.com>
2019-03-26 20:55:08 +0800

    4.寻找两个有序数组的中位数

--- src/main/java/cn/duanzx/leetcode/FindMedianSortedArrays004.java
+++ src/main/java/cn/duanzx/leetcode/FindMedianSortedArrays004.java
@@ -8,7 +8,7 @@ public class FindMedianSortedArrays004 {
      * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
      * 你可以假设 nums1 和 nums2 不会同时为空。
      * 示例 1:
-     * nums1 = [1, 3]
+     * nums1 = [1, 3]  ss
      * nums2 = [2]
      *
      * 则中位数是 2.0
