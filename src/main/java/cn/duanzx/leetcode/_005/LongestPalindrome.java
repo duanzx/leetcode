6f49cd4062022a0cc70bc382d2796899da9a442f
duanzx <15726645490@163.com>
2019-04-28 13:44:39 +0800

    005最长回文子串

--- src/main/java/cn/duanzx/leetcode/_005/LongestPalindrome.java
+++ src/main/java/cn/duanzx/leetcode/_005/LongestPalindrome.java
@@ -1,15 +1,52 @@
 package cn.duanzx.leetcode._005;
 
+import org.junit.Test;
+
 public class LongestPalindrome {
     /**
      * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
-     *     示例 1：
-     *     输入: "babad"
-     *     输出: "bab"
-     *     注意: "aba" 也是一个有效答案。
-     *     示例 2：
-     *     输入: "cbbd"
-     *     输出: "bb"
-     * */
+     * 示例 1：
+     * 输入: "babad"
+     * 输出: "bab"
+     * 注意: "aba" 也是一个有效答案。
+     * 示例 2：
+     * 输入: "cbbd"
+     * 输出: "bb"
+     */
+
+
+    @Test
+    public void test1() {
+        String s = "flsuqzhtcahnyickkgtfnlyzwjuiwqiexthpzvcweqzeqpmqwkydhsfipcdrsjkefehhesubkirhalgnevjugfohwnlhbjfewiunlgmomxkafuuokesvfmcnvseixkkzekuinmcbmttzgsqeqbrtlwyqgiquyylaswlgfflrezaxtjobltcnpjsaslyviviosxorjsfncqirsjpkgajkfpoxxmvsyynbbovieoothpjgncfwcvpkvjcmrcuoronrfjcppbisqbzkgpnycqljpjlgeciaqrnqyxzedzkqpqsszovkgtcgxqgkflpmrikksaupukdvkzbltvefitdegnlmzeirotrfeaueqpzppnsjpspgomyezrlxsqlfcjrkglyvzvqakhtvfmeootbtbwfhqucbnuwznigoyatvkocqmbtqghybwrhmyvvuchjpvjckiryvjfxabezchynfxnpqaeampvaapgmvoylyutymdhvhqfmrlmzkhuhupizqiujpwzarnszrexpvgdmtoxvjygjpmiadzdcxtggwamkbwrkeplesupagievwsaaletcuxtpsxmbmeztcylsjxvhzrqizdmgjfyftpzpgxateopwvynljzffszkzzqgofdlwyknqfruhdkvmvrrjpijcjomnrjjubfccaypkpfokohvkqndptciqqiscvmpozlyyrwobeuazsawtimnawquogrohcrnmexiwvjxgwhmtpykqlcfacuadyhaotmmxevqwarppknoxthsmrrknu";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+        String substring = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String sub = s.substring(i, j);
+                if (checkLP(sub)) {
+                    substring = sub.length() > substring.length() ? sub : substring;
+                }
+            }
+        }
+        return substring;
+    }
+
+    private boolean checkLP(String s) {
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+        for (int i = 0; i < len / 2; i++) {
+            if (arr[i] != arr[len - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
